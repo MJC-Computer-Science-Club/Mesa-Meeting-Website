9b63bc289ac75f79376f1b9326c405920a73f745
Lucas <lucaslassiter@yahoo.com>
2024-02-02 00:14:34 -0800

    Updated HomePage UI

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,35 +1,35 @@
 import React, { Component } from "react";
 import Button from 'react-bootstrap/Button';
+import { Container, Row, Col } from 'react-bootstrap';
 
 // Images
 import stupid_pic from "../../static/images/stupid_pic.png"
 
 export default class HomePage extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div>
-                <div className="container hero">
-                    <div className="row">
-                        <div className="col-md-6 mx-auto d-flex my-auto" align="center">
-                            <div>
-                                <h1>Welcome to MESA Collaborate</h1>
-                                <p>Forge study connections effortlessly. Input your preferred subjects and availability, and join your fellow MESA students for collaborative sessions. Elevate your learning experience by sharing insights and tackling challenges together. </p>
-                                <br />
-                                <a href="login">
-                                    <Button className="btn btn-primary btn-lg">Join Now</Button>
-                                </a>
-                            </div>
-                        </div>
-                        <div className="col-md-6 mx-auto" align="center">
-                            <img className="hero-image" src={stupid_pic} alt="stupid picture" />
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <Container fluid className="hero-section">
+        <Row className="justify-content-center align-items-center">
+          <Col md={6}>
+            <div className="hero-content-container">
+              <div className="hero-content">
+                <h1>Welcome to MESA Collaborate</h1>
+                <p>Forge study connections effortlessly. Input your preferred subjects and availability, and join your fellow MESA students for collaborative sessions. Elevate your learning experience by sharing insights and tackling challenges together.</p>
+                <a href="login">
+                  <Button variant="primary">Get Started</Button>
+                </a>
+              </div>
+              <div className="hero-image-col">
+                <img src={stupid_pic} alt="Hero Image" className="hero-image" />
+              </div>
             </div>
-        )
-    }
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
