b5c3bccfe68766929e9a022e57bab19bdf238c05
Lucas <lucaslassiter@protonmail.com>
2024-12-04 15:55:02 -0800

    Added functionality to POST and create users

--- rest/serializers.py
+++ rest/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ["url", "first_name", "last_name", "bio"]
+        fields = ["url", "first_name", "username", "bio"]
